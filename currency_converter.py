59c59e8adca440e784693ecda09c55b9f58209b4
Yash-Khetan <yash.khetan24@spit.ac.in>
2025-04-30 20:12:50 +0530

    Create currency_converter.py

---
+++ currency_converter.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import requests
+st.title("Live currency converter")
+amount = st.number_input("Enter the amount(INR)", min_value = 1)
+currency = st.selectbox("Convert to ",["USD","EURO","GDP","JPY"])
+
+if st.button("Convert"):
+    url = "https://api.exchangerate-api.com/v4/latest/INR"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        rates = data["rates"][currency]
+        converted_value = rates * amount
+        st.success(f"Conversion of {amount}  in {currency} is {converted_value}")
+    else:
+        st.error("Failed to fetch conversion rate!")
